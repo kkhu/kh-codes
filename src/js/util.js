0acae4431449b7859656d1fd31a3d53e43332556
brian <brian>
2019-02-14 19:10:44 +0800

    增加数字格式化方法

--- src/js/util.js
+++ src/js/util.js
@@ -1,52 +1,83 @@
-
-
+/*
+@desc 获取URL上的参数
+*/
 export function getUrlParams(name) {
-    let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i'),
-        rs = window.location.search.substr(1).match(reg);
-    return rs ? decodeURI(rs[2]) : null;
+  let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i'),
+      rs = window.location.search.substr(1).match(reg);
+  return rs ? decodeURI(rs[2]) : null;
 }
 
-export function dateFormat(date,fmt) {
-    if(!date) {
-        date = new Date();
-    }
+/*
+  @desc 日期格式化
+*/
+export function dateFormat(date, fmt) {
+  if(!date) {
+      date = new Date();
+  }
         
-    let year = date.getFullYear(),
-        month = date.getMonth() + 1,
-        month = month < 10 ? ('0'+month) : month,
-        d = date.getDate(),
-        d = d < 10 ? ('0'+d) : d,
-        hour = date.getHours(),
-        hour = hour < 10 ? ('0'+hour) : hour,
-        minutes = date.getMinutes(),
-        minutes = minutes < 10 ? ('0'+minutes) : minutes,
-        seconds = date.getSeconds(),
-        seconds = seconds < 10 ? ('0'+seconds) : seconds,
-        milliseconds = date.getMilliseconds();
+  let year = date.getFullYear(),
+      month = date.getMonth() + 1,
+      month = month < 10 ? ('0' + month) : month,
+      d = date.getDate(),
+      d = d < 10 ? ('0' + d) : d,
+      hour = date.getHours(),
+      hour = hour < 10 ? ('0' + hour) : hour,
+      minutes = date.getMinutes(),
+      minutes = minutes < 10 ? ('0' + minutes) : minutes,
+      seconds = date.getSeconds(),
+      seconds = seconds < 10 ? ('0' + seconds) : seconds,
+      milliseconds = date.getMilliseconds();
 
-        fmt = fmt ? fmt : 'yyyy-MM-dd';
-        
-        if (/yyyy/.test(fmt)) {
-            fmt = fmt.replace('yyyy', year);
-        }  
-        if (/MM/.test(fmt)) {
-            fmt = fmt.replace('MM', month);
-        } 
-        if (/dd/.test(fmt)) {
-            fmt = fmt.replace('dd',d);
-        } 
-        if (/hh/.test(fmt)) {
-            fmt = fmt.replace('hh', hour);
-        } 
-        if (/mm/.test(fmt)) {
-            fmt = fmt.replace('mm', minutes);
-        } 
-        if (/ss/.test(fmt)) {
-            fmt = fmt.replace('ss',seconds);
-        } 
-        if (/SS/.test(fmt)) {
-            fmt = fmt.replace('SS',milliseconds);
-        }  
-                
-    return fmt; 
+      fmt = fmt ? fmt : 'yyyy-MM-dd';
+      
+      if (/yyyy/.test(fmt)) {
+          fmt = fmt.replace('yyyy', year);
+      }  
+      if (/MM/.test(fmt)) {
+          fmt = fmt.replace('MM', month);
+      } 
+      if (/dd/.test(fmt)) {
+          fmt = fmt.replace('dd',d);
+      } 
+      if (/hh/.test(fmt)) {
+          fmt = fmt.replace('hh', hour);
+      } 
+      if (/mm/.test(fmt)) {
+          fmt = fmt.replace('mm', minutes);
+      } 
+      if (/ss/.test(fmt)) {
+          fmt = fmt.replace('ss',seconds);
+      } 
+      if (/SS/.test(fmt)) {
+          fmt = fmt.replace('SS',milliseconds);
+      }  
+              
+  return fmt; 
+}
+
+/*
+@desc 对数字进行分隔
+
+@param {number} num 需要处理的数字
+
+@example
+输入：2123456
+输出：2，123，456
+*/
+export function splitNum(num, splitSymbal = ',') {
+  return (num + '').replace(/\B(?=(\d{3})+(?!\d))/g, splitSymbal)
+}
+
+/*
+@desc 是否是整数
+*/
+export function isInteger(num) {
+  return num % 1 === 0
+}
+
+/*
+@desc 是否是小数
+*/
+export function isDecimal(num) {
+  return num % 1 !== 0
 }
