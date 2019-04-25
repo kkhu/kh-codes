47f386ada4cd1dc064b3a405186d26d70985fa98
brian <brian>
2019-04-25 18:58:57 +0800

    icon

---
+++ src/components/icon-cross/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <span class="icon-cross"></span>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus">
+  .icon-cross {
+    width 15px
+    height 15px
+    display flex
+    justify-content center
+    align-items center
+    line() {
+      width 100%
+      position absolute
+      content ''
+      display inline-block
+      border-top solid 0.02rem #ffffff
+    }
+    &::before {
+      line()
+      transform rotate(45deg)
+    }
+    &::after {
+      line()
+      transform rotate(-45deg)
+    }
+  }
+</style>
